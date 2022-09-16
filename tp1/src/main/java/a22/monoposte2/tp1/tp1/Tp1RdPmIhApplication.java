e526555792c68c48bb00a21f3a8090664c1f0153
ih <2063828@cegeplimoilou.ca>
2022-09-16 09:24:42 -0400

    Creation du projet Spring
    IbrahimH

---
+++ tp1/src/main/java/a22/monoposte2/tp1/tp1/Tp1RdPmIhApplication.java
@@ -0,0 +1,13 @@
+package a22.monoposte2.tp1.tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1RdPmIhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1RdPmIhApplication.class, args);
+	}
+
+}
---
+++ tp1/src/test/java/a22/monoposte2/tp1/tp1/Tp1RdPmIhApplicationTests.java
@@ -0,0 +1,13 @@
+package a22.monoposte2.tp1.tp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1RdPmIhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
