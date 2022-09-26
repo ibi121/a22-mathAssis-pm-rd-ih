84e20113dea90f51fe76ba402964dd8068428402
Rose Duval <roseduval@hotmail.com>
2022-09-26 10:51:35 -0400

    Création de la vue du générateur, ainsi que le controller de celui-ci.

--- math-assist-rd_pm_ih/modele/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/Serie.java
+++ math-assist-rd_pm_ih/modele/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/Serie.java
@@ -1,7 +1,10 @@
-package a22.climoilou.mono2.tp1.rd_pm_ih;
+package main.java.a22.climoilou.mono2.tp1.rd_pm_ih;
 
 
 
 public class Serie {
+    private int minimum;
+    private int maximum;
+    private String nom;
 
 }
---
+++ math-assist-rd_pm_ih/modele/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/generateur.java
@@ -0,0 +1,4 @@
+package main.java.a22.climoilou.mono2.tp1.rd_pm_ih;
+
+public class generateur {
+}
