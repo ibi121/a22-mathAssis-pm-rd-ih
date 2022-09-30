cf9b1c8ef076824354300121cb4d3f4a919bd9bf
Rose Duval <roseduval@hotmail.com>
2022-09-30 10:37:32 -0400

    Ajout du squelette du MainControlleur pour le FXML associé

--- math-assist-rd_pm_ih/controleur/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/MainController.java
+++ math-assist-rd_pm_ih/controleur/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/MainController.java
@@ -1,4 +1,37 @@
-package a22.climoilou.mono2.tp1.rd_pm_ih;
+package main.java.a22.climoilou.mono2.tp1.rd_pm_ih;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class MainController {
+
+    @FXML
+    private VBox vBox1;
+
+    @FXML
+    private Pane paneGauche;
+
+    @FXML
+    private HBox hBox1;
+
+    @FXML
+    private Button btnAPropos;
+
+    @FXML
+    private Pane paneDroit;
+
+    @FXML
+    private SplitPane splitPanePrincipal;
+
+    @FXML
+    private Button btnValiderSerie;
+
+    @FXML
+    private Button btnRandom;
+
 }
+
