de71f4bf4b9e3cacd8a9a9d9991b897b0496e8fe
Ibi121 <2063828@cegeplimoilou.ca>
2022-10-19 17:46:23 -0400

    Auto stash before merge of "master" and "origin/master"

--- math-assist-rd_pm_ih/controleur/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/MathAssistRdPmIhApplication.java
+++ math-assist-rd_pm_ih/controleur/src/main/java/a22/climoilou/mono2/tp1/rd_pm_ih/MathAssistRdPmIhApplication.java
@@ -11,6 +11,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan("a22.climoilou.mono2.tp1.rd_pm_ih")
 public class MathAssistRdPmIhApplication{
 
+
+	private BD bd;
+
+
+
+	@Autowired
+	public void setBd(BD bd) {
+		this.bd = bd;
+	}
+
 	public static void main(String[] args) {
 		Application.launch(ApplicationFX.class, args);
 	}
